88eb07a58e5ca557957e3c691e661ca1a8f9764e
STokarew <Sseregatokarew@gmail.com>
2021-10-09 13:58:37 +0700

    Добавьте файлы проекта.

---
+++ REC-67/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace REC_67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 1;
+            short s;
+            s = 2;
+            ushort us;
+            us = 3;
+            long l;
+            l = 4;
+            string str;
+            str = "hello";
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+            Console.WriteLine(us);
+            Console.WriteLine(l);
+            Console.WriteLine(str);
+            Console.ReadKey();
+
+        }
+    }
+}
